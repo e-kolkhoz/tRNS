f2349003f7e93346db882e36e0657221501ef8ff
e-kolkhoz <e-kolkhoz@ya.ru>
2026-02-06 03:52:52 +0400

    chore: v0.9.0.20260206-dev3

--- ESP32tRNS/version.h
+++ ESP32tRNS/version.h
@@ -1,5 +1,5 @@
 // ВРУЧНУЮ НЕ ПРАВИТЬ!, создается автоматом при запуске ./release.sh
 #ifndef VERSION_H
 #define VERSION_H
-#define FIRMWARE_VERSION "0.9.0.20260206-dev2"
+#define FIRMWARE_VERSION "0.9.0.20260206-dev3"
 #endif
